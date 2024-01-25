428ee7cca45f6151dd12d6e29eff3fdde2c5c2d1
ArrayOfLilly <arrayoflilly@gmail.com>
2024-01-25 16:17:58 +0100

    reformat

--- crawl.test.js
+++ crawl.test.js
@@ -3,52 +3,66 @@ const { normalizeURL,
         getURLsFromHTML } = require('./crawl.js')
 
 //normalizeURL tests
-test('Normalize URL: https://blog.boot.dev/something', () => {
-  expect(normalizeURL('https://blog.boot.dev/something')).toBe('blog.boot.dev/something');
+test('Normalize URL - protocol', () => {
+  const input = 'https://blog.boot.dev/something'
+  const received = normalizeURL(input)
+  const expected = 'blog.boot.dev/something'
+  expect(received).toBe(expected);
 });
 
-test('Normalize URL: http://blog.boot.dev/something/', () => {
-  expect(normalizeURL('http://blog.boot.dev/something/')).toBe('blog.boot.dev/something');
+test('Normalize URL - trailing "/"', () => {
+  const input = 'https://blog.boot.dev/something/'
+  const received = normalizeURL(input)
+  const expected = 'blog.boot.dev/something'
+  expect(received).toBe(expected);
+
 });
 
-test('Normalize URL: https://www.blog.boot.dev/something/', () => {
-  expect(normalizeURL('https://www.blog.boot.dev/something/')).toBe('blog.boot.dev/something');
-});
-
-test('Normalize URL: http://www.blog.boot.dev/something/?user=name', () => {
-  expect(normalizeURL('http://www.blog.boot.dev/something/?user=name')).toBe('blog.boot.dev/something');
-});
-
-test('Normalize URL: https://blog.boot.dev/something/?user=name#id', () => {
-  expect(normalizeURL('https://blog.boot.dev/something/?user=name#id')).toBe('blog.boot.dev/something');
-});
-
-test('Normalize URL: https://blog.boot.dev:80/something/?user=name#id', () => {
-  expect(normalizeURL('https://blog.boot.dev:80/something/?user=name#id')).toBe('blog.boot.dev/something');
-});
-
-test('Normalize URL: https://admin:admin@blog.boot.dev:80/something/?user=name#id', () => {
-  expect(normalizeURL('https://admin:admin@blog.boot.dev:80/something/?user=name#id')).toBe('blog.boot.dev/something');
+test('Normalize URL - excluded fragments: username, password, port, search, hash', () => {
+  const input = 'https://admin:admin@blog.boot.dev:80/something/?user=name#id'
+  const received = normalizeURL(input)
+  const expected = 'blog.boot.dev/something'
+  expect(received).toBe(expected);
 });
 
 
 //getURLsFromHTML test
-test('getURLsFromHTML: htmlBody="<html><head></head><body><a href="/something">Link</a></body></html>", baseURL="http://blog.boot.dev"', () => {
-  expect(getURLsFromHTML('<html><head></head><body><a href="/something">Link</a></body></html>', 'http://blog.boot.dev')).toEqual(['http://blog.boot.dev/something']);
+test('getURLsFromHTML - relative"', () => {
+  const inputHTML = '<html><head></head><body><a href="/something">Link</a></body></html>'
+  const inputBaseURL = 'http://blog.boot.dev'
+  const received = getURLsFromHTML(inputHTML, inputBaseURL)
+  const expected = ['http://blog.boot.dev/something']
+  expect(received).toEqual(expected);
 });
 
-test('getURLsFromHTML: htmlBody="<html><head></head><body><a href="/something">Link</a><a href="./another_something">Another Link</a></body></html>", baseURL="http://blog.boot.dev"', () => {
-  expect(getURLsFromHTML('<html><head></head><body><a href="/something">Link</a><a href="./another_something">Another Link</a></body></html>', 'http://blog.boot.dev')).toEqual(['http://blog.boot.dev/something', 'http://blog.boot.dev/another_something']);
+test('getURLsFromHTML - more than one, another kind relative', () => {
+  const inputHTML = '<html><head></head><body><a href="/something">Link</a><a href="./another_something">Another Link</a></body></html>'
+  const inputBaseURL = 'http://blog.boot.dev'
+  const received = getURLsFromHTML(inputHTML, inputBaseURL)
+  const expected = ['http://blog.boot.dev/something', 'http://blog.boot.dev/another_something']
+  expect(received).toEqual(expected);
 });
 
-test('getURLsFromHTML: htmlBody="<html><head></head><body><a href="http://example.com/something/">Link</a><a href="https://blog.boot.dev/something2">Link2</a><a href="/something3">Link3</a></body></html>", baseURL="http://blog.boot.dev"', () => {
-  expect(getURLsFromHTML('<html><head></head><body><a href="http://example.com/something"/>Link</a><a href="https://blog.boot.dev/something2">Link2</a><a href="/something3">Link3</a></body></html>', 'http://blog.boot.dev')).toEqual(['http://example.com/something', 'https://blog.boot.dev/something2', 'http://blog.boot.dev/something3']);
+test('getURLsFromHTML - more than one, absolute, external', () => {
+  const inputHTML = '<html><head></head><body><a href="http://example.com/something"/>Link</a><a href="https://blog.boot.dev/something2">Link2</a><a href="/something3">Link3</a></body></html>'
+  const inputBaseURL = 'http://blog.boot.dev'
+  const received = getURLsFromHTML(inputHTML, inputBaseURL)
+  const expected = ['http://example.com/something', 'https://blog.boot.dev/something2', 'http://blog.boot.dev/something3']
+  expect(received).toEqual(expected);
 });
 
-test('getURLsFromHTML: htmlBody="<html><head></head><body><a href="mailto:abc@gmail.com">Link</a></body></html>", baseURL="http://gmail.com"', () => {
-  expect(getURLsFromHTML('<html><head></head><body><a href="mailto:abc@gmail.com">Link</a></body></html>', 'http://gmail.com')).toEqual([]);
+test('getURLsFromHTML - mailto:', () => {
+  const inputHTML = '<html><head></head><body><a href="mailto:abc@gmail.com">Link</a></body></html>'
+  const inputBaseURL = 'http://gmail.com'
+  const received = getURLsFromHTML(inputHTML, inputBaseURL)
+  const expected = []
+  expect(received).toEqual(expected);
 });
 
-test('getURLsFromHTML: htmlBody="<html><head></head><body><a href="../something">Link</a></body></html>", baseURL="http://example.com"', () => {
-  expect(getURLsFromHTML('<html><head></head><body><a href="../something">Link</a></body></html>', 'http://example.com')).toEqual([]);
+test('getURLsFromHTML - relative, in parent directory tree', () => {
+  const inputHTML = '<html><head></head><body><a href="../something">Link</a></body></html>'
+  const inputBaseURL = 'http://example.com'
+  const received = getURLsFromHTML(inputHTML, inputBaseURL)
+  const expected = []
+  expect(received).toEqual(expected);
 });
